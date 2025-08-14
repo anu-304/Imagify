55962536facc4fcf56ecd74f5776e195338919ca
Anusri A <anusria304@gmail.com>
2025-08-14 11:01:40 +0530

    Login page and functionality commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,8 +7,12 @@ import Home from './pages/Home'
 import Result from './pages/Result'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import Login from './components/Login'
 
 const App = () => {
+
+  const {showLogin} = useContext(AppContext)
+
   return (
     <div className='px-4 sm:px-10 md:px-14 lg:px-28
     min-h-screen bg-gradient-to-b from-teal-50 
@@ -16,7 +20,7 @@ const App = () => {
 
 
       <Navbar/>
-      
+      {showLogin && <Login />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/result" element={<Result />} />
