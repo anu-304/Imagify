c398f5fbca1a8fff77b9a82d246a38f4f9b9d4de
Anusri A <anusria304@gmail.com>
2025-08-17 00:57:20 +0530

    user credits API commit

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -70,4 +70,24 @@ const loginUser = async (req, res) => {
     }
 }
 
-export { registerUser, loginUser }
+const userCredits = async (req, res) => {
+    try {
+        //const {userId}  = req.body
+        const userId = req.userId;
+
+        const user = await userModel.findById(userId)
+        res.json({
+            success: true,
+            credits: user.creditBalance,
+            user: {
+                name: user.name,
+            }
+        })
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message}) 
+    }
+}
+
+
+export { registerUser, loginUser, userCredits }
