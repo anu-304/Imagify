7161652731bf6b0f71145624540309e6bcf3a58f
Anusri A <anusria304@gmail.com>
2025-08-16 08:42:10 +0530

    Framer motion and onClick function in GenerateBtn section

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
 
 
   return (
-    <div className='absolute top-0 left-0 right-0
+    <div className='fixed top-0 left-0 right-0
     bottom-0 z-20 backdrop-blur-sm bg-black/30 flex 
     justify-center items-center'>
 
